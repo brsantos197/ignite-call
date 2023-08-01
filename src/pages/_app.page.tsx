1ea60a99e53ae0c82c0530cc2cdaa70589910039
Bruno Santos <brsantos197@gmail.com>
2023-08-01 07:17:49 -0300

    chore: Calendário e disponibilidade

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,8 +1,10 @@
+import '../lib/dayjs'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import { Roboto } from 'next/font/google'
 import { globalStyles } from '@/styles/global'
-
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@/lib/react-query'
 globalStyles()
 
 const roboto = Roboto({
@@ -15,10 +17,12 @@ export default function App({
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <div className={roboto.className}>
-        <Component {...pageProps} />
-      </div>
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <div className={roboto.className}>
+          <Component {...pageProps} />
+        </div>
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
